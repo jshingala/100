d803a26d5d5c03dedee55ce1fb3a816f202bdf2f
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-08 17:12:41 -0800

    ExtraFunctions

--- bill.py
+++ bill.py
@@ -6,3 +6,13 @@ if amount >=20:
     print("It is overbudget")
 else:
     print("Go for it")
+
+
+print("Odd and even Checker")
+number = int(input("Enter the integer number"))
+
+if number%2 == 0:
+    print("It is Even")
+
+else:
+    print("It is Odd")
