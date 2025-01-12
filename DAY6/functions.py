a16635329a494b13ea8828e47445a483e510053f
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-11 20:42:37 -0800

    functions

---
+++ DAY6/functions.py
@@ -0,0 +1,8 @@
+def myFunction():
+    print("Welcome to day 6")
+    numChar = (len("Hello"))
+
+myFunction()
+
+# Done rest of the code in the
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
