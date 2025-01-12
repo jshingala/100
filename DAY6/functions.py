a4684b0a1dcd77c0ca42459fc35e38367a68ecdc
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-11 22:11:46 -0800

    Completed for day 6

--- DAY6/functions.py
+++ DAY6/functions.py
@@ -6,3 +6,54 @@ myFunction()
 
 # Done rest of the code in the
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+
+'''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def hoop():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+def total():
+    if wall_in_front() == True:
+            hoop()
+    elif front_is_clear() == True:
+        move()
+while at_goal() != True:
+    total()
+'''
+
+#Hurdle 4
+'''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def round_corner():
+    turn_right()
+    move()
+    turn_right()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    round_corner()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+'''
