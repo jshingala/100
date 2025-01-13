4eb3f6824892474b0f7a1f9e4114f64e2c39f673
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-13 01:06:58 -0800

    code

--- DAY8/ceaserCipher.py
+++ DAY8/ceaserCipher.py
@@ -1,3 +1,6 @@
+import art as art 
+print(art.logo)
+
 alphabets = ['a','b','c','d','e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 direction = input("Type 'encode' to encrypt, type decode to 'decrypt':\n")
@@ -29,7 +32,7 @@ def decrypt(originalText, shiftAmount):
 def caeser(originalText, shiftAmount, encodeOrDecode):
     newLetter = ""
     for letter in originalText:
-        if letter == " ":
+        if letter not in alphabets:
             newLetter += letter
         else:
             position = alphabets.index(letter)
