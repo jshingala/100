366464e6ceb68de39d19c8757bd59b7ede2bec45
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-12 20:57:18 -0800

    functions woth parameter

---
+++ DAY8/functionparameter.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello I am Jenil Shingala  ")
+    print("I like to hike and meditate in my free time")
+
+greet()
+
+def greetWithNames(name):
+    print(f"Hi my name is {name}")
+    print(f"How are you {name}")
+
+greetWithNames("JeniL")
