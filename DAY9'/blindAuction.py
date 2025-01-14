ab95f7ed8e4c8bd49c097d5f289b033c9fe50627
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-13 21:53:02 -0800

    more functions

--- DAY9'/blindAuction.py
+++ DAY9'/blindAuction.py
@@ -1,15 +1,24 @@
+def bids(bidDict):
+    winner= ""
+    max = 0
+    for i, j in bidDict.items():
+        if j > max:
+            winner = ix
 
 
-
+bidDict = {}
 start = True
 while start:
     name = input("What is your name?:")
     bid  = int(input(" What is your bid $: "))
+    bidDict[name] = bid
+    print(bidDict)
     other= input("Are there any other bidders: If y/n").lower()
     if other == 'y':
         start = True
     elif other == 'n':
         start = False
+        dict(bidDict)
     else:
         print("Invalid Choice")
     print("\n" * 100 )
