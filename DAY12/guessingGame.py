03dfbf0e926389ed329f700786ff818f5543da32
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-19 15:30:18 -0800

    Guessing Game

--- DAY12/guessingGame.py
+++ DAY12/guessingGame.py
@@ -1,20 +1,33 @@
-import random 
+import random
 
-    
-
-
-
-
-print("Welcome to guessing game Guess a number between 1 and 100")
-level= input("Enter the mode 'e' for easy , 'm' for medium, 'h' for hard").lower()
-randomNumber = random.randint(1,100)
-
+print("Welcome to the guessing game! Guess a number between 1 and 100")
+level = input("Enter the mode: 'e' for easy, 'm' for medium, 'h' for hard: ").lower()
+randomNumber = random.randint(1, 100)  # Generate a random number
 
 if level == 'e':
-    guess =  int(input("Guess the number "))
+    tries = 5
+elif level == 'm':
+    tries = 3
+elif level == 'h':
+    tries = 1
+else:
+    print("Invalid level choice. Defaulting to easy mode.")
+    tries = 5
+
+
+while tries > 0:
+    print(f"You have {tries} guesses left.")
+    guess = int(input("Guess the number: "))
     if guess == randomNumber:
-        print("You won the game ")
+        print("Congratulations! You guessed the correct number!")
+        break  
     elif guess > randomNumber:
-        print("The number is too high")
-    elif guess < randomNumber:
-        print("The guess is too low ")
+        print("Your guess is too high.")
+    else:
+        print("Your guess is too low.")
+    tries -= 1  
+
+
+if tries == 0:
+    print(f"You lost. You are out of tries. The correct number was {randomNumber}.")
+
