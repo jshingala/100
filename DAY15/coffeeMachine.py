445cf1422176f9339189585410fced277bd8a4b7
Jenil Shingala <jenilshingala1002@gmail.com>
2025-01-21 17:23:22 -0800

    THE OTHER SOLUTION

--- DAY15/coffeeMachine.py
+++ DAY15/coffeeMachine.py
@@ -30,36 +30,33 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-def resources():
-    water = 10000
-    coffee= 100
-    Milk = 2000
-    print(f"Water ={water}ml Coffee = {coffee}g, Milk = {Milk}ml")
 
-
-
+def off():
+    print("What are the mauin reason for the inoput aws well as the output ")
 while True:
     try:
         userInput = input("Enter your choice ('e' for Espresso, 'l' for Latte, 'c' for Cappuccino and q to quit): ").strip().lower()
-
         if userInput == 'e':
             print("Indeed you are getting the espresso shots")
             break
         elif userInput == 'l':
             print("You are getting your Latte")
             break
-        elif userInput == 'c':
             print("Indeed you are getting your Cappuccino")
             break
         elif userInput == 'q':
             print("Thank you so much I hope you have a great day ahead")
             break
         elif userInput == 'r':
-            resources()
+            print(f"water {resources['water']}\n milk {resources['milk']}\ncoffee")
+        elif userInput == 'c':
+            print(f"The operating system that will be used in the operatung system {resources}")
         elif userInput == 'o':
-            print("The machine is in Mantaincec")
+            off()
             break
         else:
             raise ValueError("Invalid input. Please enter 'e', 'l', or 'c'.")
     except ValueError as e:
         print(f"Error: {e}")
+    
+    off()
